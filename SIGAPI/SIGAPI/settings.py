f3d2cd138ecd52360afa62f1c20c66311d04c4b0
Cassandra Costa <cassandra.costa10@gmail.com>
2022-11-18 15:10:09 -0300

    Corrigindo CORS

--- SIGAPI/SIGAPI/settings.py
+++ SIGAPI/SIGAPI/settings.py
@@ -15,6 +15,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -27,6 +28,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -36,16 +39,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://127.0.0.1:9000",
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-]
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_WHITELIST = [
+#    'http://localhost:3000'
+#]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
